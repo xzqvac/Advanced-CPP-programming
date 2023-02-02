21f3006c1a9b7133ea2740da59dd57538f207605
xzqvac <kawkaradoslaw@gmail.com>
2023-02-02 12:41:25 +0100

    Add threads join

--- List_6/zadanie1.cpp
+++ List_6/zadanie1.cpp
@@ -49,7 +49,7 @@ int main()
         {
             std::cerr << e.what() << "\n";
         }
-        
+        threads[i].join();        
     }
     
     std::cout << sumOfProducts << std::endl;
