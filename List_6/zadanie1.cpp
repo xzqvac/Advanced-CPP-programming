41b0ff77c9d901c6215b8cd6644de3a0faaed79d
xzqvac <kawkaradoslaw@gmail.com>
2023-02-02 12:39:22 +0100

    Create 10 threads

--- List_6/zadanie1.cpp
+++ List_6/zadanie1.cpp
@@ -8,20 +8,58 @@
 
 void dotProduct(std::vector<int> a, std::vector<int> b, std::promise<int>&& dotResultPromise)
 {   
-    dotResultPromise.set_value(std::inner_product(a.begin(), a.end(), b.begin(), 0));
+    try
+    {
+        if (a.size() == 0 || b.size() == 0)
+            throw std::logic_error("Empty vector");
+        if (a.size() != b.size())
+            throw std::logic_error("Different sizes");
+        dotResultPromise.set_value(std::inner_product(a.begin(), a.end(), b.begin(), 0));
+    }
+    catch(const std::exception& e)
+    {
+        dotResultPromise.set_exception(std::current_exception());
+    }
 } 
 
 int main()
 {
+    size_t threadsAmount = 10;
     std::vector<int> a {1, 2, 3};
     std::vector<int> b {4, 5, 6};
 
-    std::promise<int> dotProductPromise;
-    std::future<int> dotProductFuture = dotProductPromise.get_future();
+    std::vector<std::promise<int>> promises;
+    std::vector<std::future<int>> futures;
+    std::vector<std::thread> threads;
 
-    std::thread t1(dotProduct, a, b, std::move(dotProductPromise));
-    t1.join();
+    for(size_t i = 0; i < threadsAmount; i++)
+    {
+        promises.push_back(std::promise<int>());
+        futures.push_back(promises.back().get_future());
+        threads.push_back(std::thread(dotProduct, a, b, std::move(promises.back())));
+    }
 
-    std::cout << "OddSum : " << dotProductFuture.get() << std::endl;
-    return 0;
+    int sumOfProducts = 0;
+    for(size_t i = 0; i < threadsAmount; i++)
+    {
+        try{
+            sumOfProducts += futures[i].get();
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << "\n";
+        }
+        
+    }
+    
+    std::cout << sumOfProducts << std::endl;
+
+    //std::promise<int> dotProductPromise;
+    //std::future<int> dotProductFuture = dotProductPromise.get_future();
+
+    //std::thread t1(dotProduct, a, b, std::move(dotProductPromise));
+    //t1.join();
+
+    //std::cout << "OddSum : " << dotProductFuture.get() << std::endl;
+    //return 0;
 }
